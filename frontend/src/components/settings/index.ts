655e1a77d74803a812efdb5f42196be629cffc3d
sriharsh1103 <prashant.sriharsh@antiersolutions.com>
2025-05-04 20:05:14 +0530

    fix:blank

--- frontend/src/components/settings/index.ts
+++
@@ -1,5 +0,0 @@
-export { default as UserSettings } from './UserSettings';
-export { default as AccountSettings } from './AccountSettings';
-export { default as SecuritySettings } from './SecuritySettings';
-export { default as NotificationSettings } from './NotificationSettings';
-export { default as HelpSupport } from './HelpSupport'; 
